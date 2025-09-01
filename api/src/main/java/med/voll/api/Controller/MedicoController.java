7d21d0f87973304f59754e2de2d572fd4fcdf1f5
Victor Hugo Fedatto <victorhugofedatto@gmail.com>
2025-09-01 15:52:34 -0300

    Service&Controller

--- api/src/main/java/med/voll/api/Controller/MedicoController.java
+++ api/src/main/java/med/voll/api/Controller/MedicoController.java
@@ -1,13 +1,18 @@
 package med.voll.api.Controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import med.voll.api.Service.DadosCadastroMedico;
 
 @RestController //Não é mais classe Java -> Quem vai gerenciar agora é o Spring
 @RequestMapping("medicos") //Requisição para "/medicos" ele sabe para onde mandar.
 public class MedicoController {
     
-    public void cadastrar(){
-        
+    @PostMapping
+    public void cadastrar(@RequestBody DadosCadastroMedico dados){
+        System.out.println(dados);
     }
 }
---
+++ api/src/main/java/med/voll/api/Service/DadosCadastroMedico.java
@@ -0,0 +1,5 @@
+package med.voll.api.Service;
+
+public record DadosCadastroMedico() {
+    
+}
